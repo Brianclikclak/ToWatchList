2c97421a417ce70e84545e99a95f66c870b64d26
Brian Menendez <Brianclikclak@hotmail.com>
2023-08-17 14:37:56 -0700

    creating entity

---
+++ demo/src/main/java/com/example/demo/models/Film.java
@@ -0,0 +1,59 @@
+package com.example.demo.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Films")
+public class Film {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title", nullable = false, unique = true, length = 50)
+    private String title;
+
+    @Column(name = "date", nullable = false, length = 50)
+    private int date;
+
+    @Column(name = "genre", nullable = false)
+    private String genre;
+
+    public Film(Long id, String title, int date, String genre) {
+        super();
+        this.id = id;
+        this.title = title;
+        this.date = date;
+        this.genre = genre;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTittle(String title) {
+        this.title = title;
+    }
+
+    public int getDate() {
+        return date;
+    }
+
+    public void setDate(int date) {
+        this.date = date;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+}
