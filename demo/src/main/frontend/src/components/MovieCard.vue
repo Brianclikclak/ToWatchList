5680a859c8e0a91b53d6016972fe74f2b7220137
Santiago <esematillajr@gmail.com>
2023-08-21 15:07:05 +0200

    card created

--- demo/src/main/frontend/src/components/MovieCard.vue
+++ demo/src/main/frontend/src/components/MovieCard.vue
@@ -4,6 +4,22 @@
 
 <template>
 
+    <div class="card text-center w-85 m-3">
+        <div class="card-header">
+            Title
+        </div>
+        <div class="card-body ">
+            <p class="card-text" id="year">Year</p>
+            <p class="card-text" id="genre">Genre</p>
+            <p class="card-text" id="whereToWatch">Where to watch</p>
+            
+        </div>
+        <div class="card-footer text-muted d-flex justify-content-evenly">
+            <a href="#" class="btn btn-primary">Edit</a>
+            <a href="#" class="btn btn-danger">Delete</a>
+        </div>
+    </div>
+
 </template>
 
 <style>
