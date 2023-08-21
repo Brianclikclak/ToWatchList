354d83df4df3bf3e22af5adbdab4be9f1ab4de75
Brianclikclak <brianclikclak@hotmail.com>
2023-08-21 15:51:13 +0200

    renamed

---
+++ demo/src/main/frontend/src/components/InputForm.vue
@@ -0,0 +1,26 @@
+<script setup>
+</script>
+
+<template>
+  <form>
+  <div class="form-group">
+    <label for="formGroupInput1"></label>
+    <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Title">
+  </div>
+  <div class="form-group">
+    <label for="formGroupInput2"></label>
+    <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Year">
+  </div>
+  <div class="form-group">
+    <label for="formGroupInput3"></label>
+    <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Genre">
+  </div>
+  <div class="form-group">
+    <label for="formGroupInput4"></label>
+    <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Where to watch">
+  </div>
+</form>
+</template>
+<style scoped>
+
+</style>
