b7639168c9b590a2c5dd32240ba7ed6b1e6ea4e8
Brian Menendez <Brianclikclak@hotmail.com>
2023-08-24 11:32:58 -0700

    methods logic added

--- demo/src/main/java/com/example/demo/services/FilmServiceImpl.java
+++ demo/src/main/java/com/example/demo/services/FilmServiceImpl.java
@@ -1,23 +1,49 @@
 package com.example.demo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.demo.models.Film;
-import com.example.demo.repository.FilmRespository;
+import com.example.demo.repository.FilmRepository;
 
 @Service
 public class FilmServiceImpl implements FilmService {
 
     @Autowired
-    private FilmRespository repository;
+    private FilmRepository filmrepository;
 
     @Override
-    public List<Film> listAllFilms() {
 
-        return repository.findAll();
+    @Transactional
+    public List<Film> listAllFilms() {
+        return filmrepository.listAllFilms();
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Iterable<Film> findAll() {
+        return filmrepository.findAll();
+    }
+
+    @Override
+    @Transactional
+    public Optional<Film> findById(Long id) {
+        return filmrepository.findById(id);
+    }
+
+    @Override
+    @Transactional
+    public Film save(Film film) {
+        return filmrepository.save(film);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        filmrepository.deleteById(id);
     }
 
 }
