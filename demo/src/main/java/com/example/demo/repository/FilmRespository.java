34e0f8cb87996a71be29440558f5faed53fe2a0a
Brian Menendez <Brianclikclak@hotmail.com>
2023-08-24 11:33:24 -0700

    update

--- demo/src/main/java/com/example/demo/repository/FilmRespository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.models.Film;
-
-public interface FilmRespository extends JpaRepository<Film, Long> {
-
-}
