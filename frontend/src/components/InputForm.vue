1273c0c8790ab8cbed139bfb29998e8d3d727f16
Brianclikclak <brianclikclak@hotmail.com>
2023-08-28 11:40:37 +0200

    method post created, need to be fixed

--- frontend/src/components/InputForm.vue
+++ frontend/src/components/InputForm.vue
@@ -1,24 +1,64 @@
 <script setup>
+import FilmDataService from '../services/FilmDataService';
+import {reactive } from 'vue';
+
+
+const film = reactive({
+    id: null,
+    title: "",
+    date: "",
+    genre: ""
+})
+function addNewFilm() {
+    let data = {
+        title: film.title,
+        date: film.date,
+        genre: film.genre,
+        director: film.director
+    };
+    FilmDataService.create(data)
+        .then(response => {
+            film.id = response.data.id;
+            console.log(response.data);
+        })
+        .catch(e => {
+            console.log(e);
+        });
+}
+
+/* const newFilm = ref();
+
+function createNewFilm() {
+    FilmDataService.create(newFilm.value)
+    .then(response =>{
+
+        console.log(response.data);
+        newFilm.value.title = response.data.tittle
+        newFilm.value.date = response.data.date
+        newFilm.value.genre = response.data
+    })
+} */
 </script>
 
 <template>
   <form>
   <div class="form-group">
     <label for="formGroupInput1"></label>
-    <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Title">
+    <input type="text" class="form-control" id="title" placeholder="Title" v-model="film.title">
   </div>
   <div class="form-group">
     <label for="formGroupInput2"></label>
-    <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Year">
+    <input type="text" class="form-control" id="date" placeholder="Year" v-model="film.date">
   </div>
   <div class="form-group">
     <label for="formGroupInput3"></label>
-    <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Genre">
+    <input type="text" class="form-control" id="genre" placeholder="Genre" v-model="film.genre">
   </div>
   <div class="form-group">
     <label for="formGroupInput4"></label>
-    <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Where to watch">
+    <input type="text" class="form-control" id="director" placeholder="Where to watch" v-model="film.director">
   </div>
+  <button @click="addNewFilm"> Add movie </button>
 </form>
 </template>
 <style scoped>
