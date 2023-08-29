962bcd1c9f67ef33d48760a2b3d2545998400797
Brianclikclak <brianclikclak@hotmail.com>
2023-08-29 11:26:21 +0200

    implement new routes

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,12 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import EditCard from '../components/EditCard.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
+      name: 'Homeview',
       component: HomeView
     },
     {
@@ -16,6 +17,11 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/edit/:id',
+      name:'EditCard',
+      component: EditCard
     }
   ]
 })
