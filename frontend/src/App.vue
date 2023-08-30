784d8fe9f1214f12707897b5e9d992778935b844
victorteje <vteje2@gmail.com>
2023-08-30 11:03:47 +0200

    fixed gradient

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -9,6 +9,16 @@
 
 <style scoped>
     body {
-        background: linear-gradient(97deg, rgba(2,0,36,1) 0%, rgba(9,12,121,0.8680089485458613) 46%, rgba(0,212,255,1) 100%); 
+        height: 100vh;
+        background: linear-gradient(97deg, rgba(2,0,36,1) 0%, rgba(9,12,121,0.8) 46%, rgba(0,212,255) 100%); 
+        background-attachment: fixed;
+        background-size: cover;
+        background-repeat: repeat;
+    }
+
+    @media only screen and (max-width: 1700px) {
+        body {
+            height: 100%;
+        }
     }
 </style>
