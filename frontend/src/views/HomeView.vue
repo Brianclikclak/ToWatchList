76fe6b2e5804e8016a6b9703850e74cf1fb3bc0d
victorteje <vteje2@gmail.com>
2023-08-30 09:48:14 +0200

    fixed home view

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,28 +1,21 @@
 <script setup>
-import MovieCard from '../components/MovieCard.vue';
-import InputForm from '../components/InputForm.vue';
+  import MovieCard from '../components/MovieCard.vue';
+  import InputForm from '../components/InputForm.vue';
 </script>
 
+
 <template>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/image/logo.svg"/>
+  </header>
   
   <main>
-    <header>
-    <img alt="Vue logo" class="logo" src="@/assets/image/logo.svg"/>
-   </header>
-    
-      
-      
-      <InputForm/>
-      <div class="MoviesWatch">
-        <RouterLink to="/">Unwatched Movies</RouterLink>
-        <RouterLink to="/about">Watched Movies</RouterLink>
-     </div>
-      <MovieCard/>
-      
-    
-  
+    <InputForm/>
+    <MovieCard/> 
   </main>
 </template>
+
+
 <style scoped>
 .MoviesWatch{
   display: flex;
